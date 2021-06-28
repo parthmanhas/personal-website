06dd95f607622fc0767f87f5b0747a45028d83cc
parthmanhas <parthmanhas@gmail.com>
2021-06-28 08:31:44 +0530

    before componentization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import './App.scss';
 
 function App() {
+
+  window.addEventListener('scroll', (ev) => console.log(ev))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+    <header className='navbar'>
+        <div className='navbar__container'>
+        <div className='navbar__container--left'>
+          <ul>
+            <li>Logo</li>
+            <li>Title</li>
+          </ul>
+        </div>
+        <div className='navbar__container--right'>
+          <ul>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
       </header>
-    </div>
+      <section className='intro'>
+        <div className='intro__container--animation'>
+          <h1>intro</h1>
+        </div>
+      </section>
+      <section className='projects'>
+          <div className='projects__item'>
+            <h1>Project 1</h1>
+          </div>
+          <div className='projects__item'>
+            <h1>Project 2</h1>
+          </div>
+          <div className='projects__item'>
+            <h1>Project 3</h1>
+          </div>
+          <div className='projects__item'>
+            <h1>Project 4</h1>
+          </div>
+      </section>
+      <footer className='footer'>
+        <ul>
+          <li>
+            Instagram
+          </li>
+          <li>
+            Reddit
+          </li>
+          <li>
+            LinkedIn
+          </li>
+        </ul>
+      </footer>
+    </>
   );
 }
 
